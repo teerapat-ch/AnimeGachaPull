73c8f16b01f6a7303213d415dfb5ea0e1c3f09c7
teerapat-ch <coptee413@gmail.com>
2026-04-09 13:37:43 +0700

    feat: github repo button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Button } from "./components/ui/button"
 import AnimeCardList from "./components/AnimeCardList"
+import { Slider } from "./components/ui/slider"
 
 interface AnimeResponse {
   results: {
@@ -14,16 +15,25 @@ interface AnimeResponse {
 const App = () => {
 
   const [data, setData] = useState<AnimeResponse>()
-  const [amount, setAmount] = useState<number>(5)
+  const [amount, setAmount] = useState<number[]>([5])
 
   const fetchAnime = async () => {
-    const res = await fetch('https://nekos.best/api/v2/waifu?amount=5')
+    const res = await fetch(`https://nekos.best/api/v2/waifu?amount=${amount[0]}`)
     const json: AnimeResponse = await res.json()
     setData(json)
   }
 
   return (
     <div className="flex flex-col items-center m-4">
+      <h1 className="mb-2">Pull amount: {amount}</h1>
+      <Slider
+        defaultValue={[5]}
+        min={1}
+        max={10}
+        step={1}
+        onValueChange={(val) => setAmount(val)}
+        className="w-1/8 mb-4"
+      />
       <Button onClick={() => fetchAnime()} className="px-6 py-6 mb-4 hover:cursor-pointer w-30 bg-gray-600">
         <h1 className="text-4xl">Pull</h1>
       </Button>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,17 @@
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-center w-full p-4 shadow-md">
+    <div className="grid grid-cols-3 justify-items-center w-full p-4 shadow-md">
+      <div />
       <h1 className="text-2xl font-semibold">Anime Gacha Pull</h1>
+      <div className="flex justify-end w-full">
+        <a href="https://github.com/teerapat-ch/AnimeGachaPull">
+          <img
+            src="github.png"
+            alt="github"
+            width={30}
+          />
+        </a>
+      </div>
     </div>
   )
 }
